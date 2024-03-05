a4f6bd7817a8955332e3c0fc76099b07616bdc20
Slaitroc <92638923+Slaitroc@users.noreply.github.com>
2024-03-05 16:18:59 +0100

    GC31 Project Upload

---
+++ src/test/java/it/polimi/ingsw/gc31/HelloApplicationTest.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.gc31;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloApplicationTest {
+    @Test
+    void test_somma() {
+        assertEquals(4, HelloApplication.test_prova(2));
+
+    }
+}
