4bad3fcedc4649ae0c85148b18f2889a5b7293d7
Slaitroc <92638923+Slaitroc@users.noreply.github.com>
2024-03-12 15:30:53 +0100

    UML Translation Board, GameModel, Player

--- src/main/java/it/polimi/ingsw/gc31/Model/Board.java
+++ src/main/java/it/polimi/ingsw/gc31/Model/Board.java
@@ -1,5 +1,14 @@
 package it.polimi.ingsw.gc31.Model;
 
+import it.polimi.ingsw.gc31.Model.Deck.*;
+
 public class Board {
+
+    private GoldDeck goldDeck;
+    private ResourceDeck resourceDeck;
+    private ObjectiveDeck objectiveDeck;
+    
+    public Board(){}
+    
     
 }
--- src/main/java/it/polimi/ingsw/gc31/Model/GameModel.java
+++ src/main/java/it/polimi/ingsw/gc31/Model/GameModel.java
@@ -1,5 +1,21 @@
 package it.polimi.ingsw.gc31.Model;
 
+import java.util.List;
+
+import it.polimi.ingsw.gc31.Model.Player.Player;
+import it.polimi.ingsw.gc31.Model.Deck.Deck;
+
+
 public class GameModel {
     
+    private Board board;
+    private List<Player> players;
+    private Player playingPlayer;
+
+    public GameModel(List<String> nicknames){}
+    /* public void beginEndGame(){} */
+    private void pick(Player p, Deck d){}
+    private boolean checkPoints(){return false;}
+
+
 }
