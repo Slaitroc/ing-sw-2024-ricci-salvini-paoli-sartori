d32a139c94c591f4402f9685dc137d31624750e3
salvo <salvoc878@gmail.com>
2024-03-14 21:48:08 +0100

    Add toString method to Color

--- src/main/java/it/polimi/ingsw/gc31/Model/Enum/Color.java
+++ src/main/java/it/polimi/ingsw/gc31/Model/Enum/Color.java
@@ -1,12 +1,36 @@
 package it.polimi.ingsw.gc31.Model.Enum;
 
+/**
+ * Enum that represents every color inside the game
+ */
 public enum Color {
-    RED,
-    GREEN,
-    BLUE,
-    BLACK,
-    YELLOW,
-    PURPLE,
-    NOCOLOR
-    
+    RED("Red"),
+    GREEN("Green"),
+    BLUE("Blue"),
+    BLACK("Black"),
+    YELLOW("Yellow"),
+    PURPLE("Purple"),
+    NOCOLOR("NoColor"),
+    ;
+    /**
+     * it is the string representation of a Color object
+     */
+    private final String stringName;
+
+    /**
+     * when a Color object is created the construtor automatically set the string representation based on the value
+     * of the object
+     */
+    Color(String stringName) {
+        this.stringName = stringName;
+    }
+
+    /**
+     * used to get the string representation of a Color object
+     * @return stringName
+     */
+    @Override
+    public String toString() {
+        return stringName;
+    }
 }
