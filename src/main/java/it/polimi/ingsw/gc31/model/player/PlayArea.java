3434a90ef5063d87352457e778503b3fee90f6ad
Krotox <matteog.paoli@gmail.com>
2024-03-19 13:12:48 +0000

    Passaggio copia di mappe Controllo costruttore di player

--- src/main/java/it/polimi/ingsw/gc31/model/player/PlayArea.java
+++ src/main/java/it/polimi/ingsw/gc31/model/player/PlayArea.java
@@ -1,11 +1,5 @@
 package it.polimi.ingsw.gc31.model.player;
-
 import java.awt.Point;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
 import it.polimi.ingsw.gc31.model.card.PlayableCard;
 import it.polimi.ingsw.gc31.model.enumeration.Resources;
 import java.util.HashMap;
@@ -20,60 +14,57 @@ public class PlayArea {
     // unnecessary anyway
     private Map<Resources, Integer> achievedResources;
 
-    PlayArea() {
+    PlayArea(){
     }
 
-    // Create the HashMap, place starter at (0,0)
-    // Create a hashMap for the achievedResources and
-    // Add the resources to the map itself
-    // It could be done calling UpdateAvailableRes, but it would be less efficient
+
+    //Create the HashMap, place starter at (0,0)
+    //Create a hashMap for the achievedResources and
+    //Add the resources to the map itself
+    //It could be done calling UpdateAvailableRes, but it would be less efficient
     // and unnecessary being the first card placed
-    public void placeStarter(PlayableCard card) {
+    public void placeStarter(PlayableCard card){
         placedCards = new HashMap<>();
         Point point = new Point(0, 0);
         placedCards.put(point, card);
         this.achievedResources = new HashMap<>();
-        for (Resources r : card.getResources()) {
-            achievedResources.put(r, achievedResources.get(r) + 1);
+        for (Resources r: card.getResources()){
+            achievedResources.put(r, achievedResources.get(r)+1);
         }
     }
 
     // this method creates a set of keys from the requirements read from the card
-    // than it proceed to slide them with a for to verify that in the map of
-    // achieved resources,
+    // than it proceed to slide them with a for to verify that in the map of achieved resources,
     // I have enough of them
-    private boolean checkRequirements(PlayableCard card) {
+    private boolean checkRequirements (PlayableCard card){
         Set<Resources> RequiredRes = card.getRequirements().keySet();
-        for (Resources r : RequiredRes) {
-            if (achievedResources.get(r) == null)
-                return false;
-            if (card.getRequirements().get(r) < achievedResources.get(r))
-                return false;
+        for(Resources r: RequiredRes){
+            if(achievedResources.get(r)==null) return false;
+            if (card.getRequirements().get(r) < achievedResources.get(r)) return false;
         }
         return true;
     }
 
     // Firstly it checks out if I have enough Resources to play the card.
-    // Then it adds the card in the placedCard Map if the function allowedMove
-    // return true.
-    // Then return the value of points gained from that card
-    // Notice that player will have to call:
+    //Then it adds the card in the placedCard Map if the function allowedMove return true.
+    //Then return the value of points gained from that card
+    //Notice that player will have to call:
     // score += hisPlayArea.place(card, point) to adds points at his score correctly
-    public int place(PlayableCard card, Point point) {
+    public int place(PlayableCard card, Point point){
         if (checkRequirements(card)) {
             if (allowedMove(point)) {
                 placedCards.put(point, card);
                 updateAvailableRes(card, point);
             }
         }
-        if (card.getObjective() != null)
+        if (card.getObjective()!=null)
             return card.getObjective().isObjectiveDone(placedCards, point);
         return card.getScore();
     }
 
     /*
      * //skeleton of the quickMoveCheck (unnecessary)
-     * 
+     *
      * private boolean quickMoveCheck(Point point){
      * if (point.x >= playAreaLimit[0] || point.x <= playAreaLimit[2]) {
      * if (point.y >= playAreaLimit[1] || point.y >= playAreaLimit[3])
@@ -141,11 +132,9 @@ public class PlayArea {
         newPoint.x = point.x + 1;
         newPoint.y = point.y + 1; // coordinates of the card in the NorthEst position of the one I am placing
         if (placedCards.get(newPoint) != null) {
-            r = placedCards.get(newPoint).getResources().get(2); // Assign to r the value of the Resources that im
-                                                                 // covering
-            achievedResources.put(r, achievedResources.get(r) - 1); // Decrement the number of that given resource in
-                                                                    // the map
-            placedCards.get(newPoint).coverCorner(2); // CoverCorner of the card
+            r = placedCards.get(newPoint).getResources().get(2);  //Assign to r the value of the Resources that im covering
+            achievedResources.put(r, achievedResources.get(r) - 1); //Decrement the number of that given resource in the map
+            placedCards.get(newPoint).coverCorner(2);  //CoverCorner of the card
         }
 
         // Covering SouthEast
@@ -176,12 +165,13 @@ public class PlayArea {
         }
     }
 
-    public Map<Point, PlayableCard> getPlacedCards() {
-        return this.placedCards;
+
+    public Map<Point, PlayableCard> getPlacedCards(){
+        return new HashMap<>(placedCards);
     }
 
-    public Map<Resources, Integer> getAchievedResources() {
-        return this.achievedResources;
+    public Map<Resources, Integer> getAchievedResources(){
+            return new HashMap<>(achievedResources);
     }
 
 }
--- src/main/java/it/polimi/ingsw/gc31/model/player/Player.java
+++ src/main/java/it/polimi/ingsw/gc31/model/player/Player.java
@@ -1,7 +1,7 @@
 package it.polimi.ingsw.gc31.model.player;
 import java.util.Scanner;  // Import the Scanner class to test moveCardInHand
 
-import it.polimi.ingsw.gc31.model.card.Card;
+import it.polimi.ingsw.gc31.model.card.PlayableCard;
 import it.polimi.ingsw.gc31.model.card.ObjectiveCard;
 import it.polimi.ingsw.gc31.model.enumeration.Color;
 
@@ -12,8 +12,9 @@ public class Player {
     private String username;
     private PlayArea playArea;
     private final Color pawnColor;
-    private List<Card> hand;
+    private List<PlayableCard> hand;
     private ObjectiveCard objectiveCard;
+    private PlayerState inGameState;
     protected int score;
 
 
@@ -21,25 +22,44 @@ public class Player {
         this.pawnColor = color;
         this.username = username;
         playArea = new PlayArea();
-        hand = new ArrayList<Card>();
+        hand = new ArrayList<PlayableCard>();
         score = 0;
     }
 
-    public boolean addToHand(Card card){
-        return true;//FIX
-
+    //Really Necessary?
+    public boolean addToHand(PlayableCard card){
+        this.hand.add(card);
+    return true;
     }
-    public void moveCardInHand(){
 
+    //Basic repositioning of the card in hand implemented temporarily with an input output System
+    public void moveCardInHand(){
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("Insert position of the first card [1-3]: ");
+        int cardPosition1 = myScanner.nextInt();
+        PlayableCard card1 = hand.get(cardPosition1);
+
+        System.out.println("Insert position of the second card [1-3]: ");
+        int cardPosition2 = myScanner.nextInt();
+        PlayableCard card2 = hand.get(cardPosition2);
+
+        this.hand.set(cardPosition1, card2);
+        this.hand.set(cardPosition2, card1);
+        System.out.println("New Hand disposition: " + hand);
     }
     public void changeState(){
-
+        //??
     }
     public void addObjectiveCard(ObjectiveCard card){
-
+        this.objectiveCard = card;
     }
     public int getScore(){
         return this.score;
+    }
+
+    public PlayArea getPlayArea(){
+        return playArea;
     }
 
 }
