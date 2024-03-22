1bc460ee274912adb0574ce4eda9a2246b150303
salvo <salvoc878@gmail.com>
2024-03-22 17:43:15 +0100

    Add some comments

--- src/main/java/it/polimi/ingsw/gc31/model/card/CardFront.java
+++ src/main/java/it/polimi/ingsw/gc31/model/card/CardFront.java
@@ -17,14 +17,39 @@ import java.util.*;
  * @author Christian Salvini
  */
 public class CardFront {
+    /**
+     * The score obtained from placing the card.
+     */
     private final int score;
+    /**
+     * List of Resources that represents the corners of the card's Front. The corners are identified according
+     * to the following convention:
+     *      0-> Up Dx
+     *      1-> Down Dx
+     *      2-> Down Sx
+     *      3-> Up Sx
+     * It can have a maximum of 4 elements.
+     */
     private final List<Resources> resources;
+    /**
+     * The resources the Player must have in their playArea to be able to place the card.
+     */
     private final Map<Resources, Integer> requirements;
+    /**
+     * link to the image of the front side
+     */
     private final String dirImg;
+    /**
+     * Objective of the card to be verified in order to obtain the score points.
+     */
     private final Objective ob;
 
+    /**
+     * Constructor of the class
+     */
     public CardFront(int score, List<Resources> resources, Map<Resources, Integer> requirements, String dirImg, Objective ob)
-            //throws WrongNumberOfCornerException, DirImgValueMissingException
+            //throws WrongNumberOfCornerException
+            //, DirImgValueMissingException
     {
         this.score = score;
 
@@ -38,10 +63,20 @@ public class CardFront {
         this.ob = ob;
     }
 
+    /**
+     * Check if it's possible to place a card on the corner indicated by the parameter Corner
+     * @param corner corner to be checked
+     * @return true if it is possible to place a card on that corner, false otherwise
+     */
     public boolean checkCorner(int corner) {
         return resources.get(corner) != Resources.HIDDEN;
     }
 
+    /**
+     *
+     * @param corner
+     * @return
+     */
     public Resources coverCorner(int corner) {
         Resources ret = resources.get(corner);
         resources.set(corner, Resources.HIDDEN);
