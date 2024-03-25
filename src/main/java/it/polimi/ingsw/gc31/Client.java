87d74f00f2d363ad34f74f3670d8a73fe92878c2
Slaitroc <92638923+Slaitroc@users.noreply.github.com>
2024-03-25 14:03:21 +0100

    Refactor Client

--- src/main/java/it/polimi/ingsw/gc31/Client.java
+++ src/main/java/it/polimi/ingsw/gc31/Client.java
@@ -2,29 +2,49 @@ package it.polimi.ingsw.gc31;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Paint;
+import javafx.scene.paint.RadialGradient;
 import javafx.stage.Stage;
 
+import java.awt.Color;
 import java.io.IOException;
 
 public class Client extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Client.class.getResource("Views/start-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 643, 374);
+
+        // Stage stage = new Stage();
+        Group root = new Group();
+        Scene scene = new Scene(root, Paint.valueOf("Black"));
+
         stage.getIcons().add(new Image(Client.class.getResourceAsStream("AppIcons/icon.png")));
-        stage.setTitle("CODEX Naturalis-30L Version");
+        stage.setTitle("CODEX Naturalis");
+
+        // stage.setFullScreen(true); // non funziona sul mio pc
+        stage.setWidth(800);
+        stage.setHeight(700);
+
+        // stage.setResizable(false);
+
         stage.setScene(scene);
         stage.show();
     }
 
+    // FXMLLoader fxmlLoader = new
+    // FXMLLoader(Client.class.getResource("Views/start-view.fxml"));
+    // Scene scene = new Scene(fxmlLoader.load(), 643, 374);
+    // stage.getIcons().add(new
+    // Image(Client.class.getResourceAsStream("AppIcons/icon.png")));
+    // stage.setTitle("CODEX Naturalis-30L Version");
+    // stage.setScene(scene);
+    // stage.show();
+
     public static void main(String[] args) {
         launch();
-        System.out.println("Ciao sono il client");
     }
 
-    public static int test_prova(int x){
-        return x*x;
-    }
 }
--- src/test/java/it/polimi/ingsw/gc31/HelloApplicationTest.java
+++ src/test/java/it/polimi/ingsw/gc31/HelloApplicationTest.java
@@ -2,13 +2,8 @@ package it.polimi.ingsw.gc31;
 
 import org.junit.jupiter.api.Test;
 
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class HelloApplicationTest {
-    @Test
-    void test_somma() {
-        assertEquals(4, Client.test_prova(2));
 
-    }
 }
