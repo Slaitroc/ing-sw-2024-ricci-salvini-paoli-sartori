1fc21e5012b8fc1293df7725684b6b51feb02df2
AleSarto <160670901+AleSarto@users.noreply.github.com>
2024-03-26 15:52:31 +0100

    ResourceScoreTest solved

--- src/test/java/it/polimi/ingsw/gc31/model/strategies/ResourceScoreTest.java
+++ src/test/java/it/polimi/ingsw/gc31/model/strategies/ResourceScoreTest.java
@@ -33,10 +33,77 @@ class ResourceScoreTest {
      * This method tests if the result of ResourceScore is correct with a certain Resources contained on the board
      */
     @Test
-    void firstTest() {
+    void equalsResourceNumberTest() {
         Objective ob = new ResourceScore(Resources.INK);
+        PlayableCard card;
+        point.move(0, 0);
+        // Piazzo sul campo la prima starter card presente nel file pdf
+        //playArea.place(createStarterCard(Resources.PLANT, Resources.ANIMAL,
+        //                Resources.INSECT, Resources.MUSHROOM, Resources.PLANT, Resources.EMPTY,
+        //                Resources.INSECT, Resources.EMPTY, Resources.INSECT ), point);
+        playArea.placeStarter(createStarterCard(Resources.PLANT, Resources.ANIMAL,
+                Resources.INSECT, Resources.MUSHROOM, Resources.PLANT, Resources.EMPTY,
+                Resources.INSECT, Resources.EMPTY, Resources.INSECT ));
+        //piazzo le altre carte
+        point.move(1, -1);
+        card = createResourceCard(Resources.EMPTY, Resources.HIDDEN, Resources.MUSHROOM,
+                Resources.MUSHROOM, Resources.EMPTY, Resources.EMPTY, Resources.EMPTY, Resources.EMPTY,
+                Resources.MUSHROOM);
+//        boolean flag = card.getSide();
+        card.changeSide();
+        playArea.place(card, point);
+        point.move(2, 0);
+        card = createResourceCard(Resources.INSECT, Resources.EMPTY, Resources.SCROLL, Resources.MUSHROOM,
+                Resources.EMPTY, Resources.EMPTY, Resources.EMPTY, Resources.EMPTY, Resources.MUSHROOM);
+        card.changeSide();
+        playArea.place(card, point);
+        point.move(3, 1);
+        card = createResourceCard(Resources.MUSHROOM, Resources.ANIMAL, Resources.HIDDEN, Resources.INK,
+                Resources.EMPTY, Resources.EMPTY, Resources.EMPTY, Resources.EMPTY, Resources.MUSHROOM);
+        card.changeSide();
+        playArea.place(card, point);
 
+        System.out.println(playArea.getAchievedResources());
+        assertEquals(1, ob.isObjectiveDone(playArea.getPlacedCards(), point, playArea.getAchievedResources()));
+    }
 
+    private  PlayableCard createStarterCard(Resources f1, Resources f2, Resources f3, Resources f4, Resources b1,  Resources b2,  Resources b3,  Resources b4, Resources b5){
+        int score = 0;
+
+        List<Resources> resourceFront = new ArrayList<>();
+        resourceFront.add(f1);
+        resourceFront.add(f2);
+        resourceFront.add(f3);
+        resourceFront.add(f4);
+
+        Map<Resources, Integer> requirements = new HashMap<>();
+
+        String dirImgFront = null;
+        Objective ob = null;
+
+        CardFront front = new CardFront(
+                score,
+                resourceFront,
+                requirements,
+                dirImgFront,
+                ob
+        );
+
+        List<Resources> resourceBack = new ArrayList<>();
+        resourceBack.add(b1);
+        resourceBack.add(b2);
+        resourceBack.add(b3);
+        resourceBack.add(b4);
+        resourceBack.add(b5);
+
+        String dirImgBack = null;
+
+        CardBack back = new CardBack(
+                resourceBack,
+                dirImgBack
+        );
+
+        return new StarterCard(front, back);
     }
 
     private PlayableCard createGoldCard(Resources f1, Resources f2, Resources f3, Resources f4,
