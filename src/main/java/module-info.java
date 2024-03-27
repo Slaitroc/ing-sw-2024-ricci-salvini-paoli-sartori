7fae17ad743042682b144994b04808ada80d3961
salvo <salvoc878@gmail.com>
2024-03-27 18:47:15 +0100

    Added rmi dependecy

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,8 +6,10 @@ module it.polimi.ingsw.gc31 {
     requires org.controlsfx.controls;
     requires java.desktop;
     requires com.google.gson;
+    requires java.rmi;
 
     opens it.polimi.ingsw.gc31.view to javafx.fxml;
 
     exports it.polimi.ingsw.gc31;
+    exports it.polimi.ingsw.gc31.rmi;
 }
