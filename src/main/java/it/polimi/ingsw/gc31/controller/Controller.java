6087a2984deeff401c083d8e47680c17e6481099
salvo <salvoc878@gmail.com>
2024-04-05 01:30:46 +0200

    Remove Controller Interface

--- src/main/java/it/polimi/ingsw/gc31/controller/Controller.java
+++ src/main/java/it/polimi/ingsw/gc31/controller/Controller.java
@@ -19,7 +19,7 @@ import java.awt.Color;
 //il GameController relativo al primo match viene creato subito dopo che il primo player si è loggato? 
 //Mi sembra più semplice fare così che gestire le attese per la creazione dei GameController nel Controller 
 
-public class Controller implements ControllerInterface {
+public class Controller{
     // gson per serializzare i dati da inviare al client
     private Gson gson;
     private final GameController gameController;
@@ -58,7 +58,6 @@ public class Controller implements ControllerInterface {
         return clients.size();
     }
 
-    @Override
     public void joinGame(String username, VirtualView client) throws RemoteException {
         gameController.addPlayer(username);
         System.out.println("Added players: " + username);
@@ -67,7 +66,6 @@ public class Controller implements ControllerInterface {
             this.initGame();
         }
     }
-    @Override
     public void getHand(String username) throws RemoteException {
         List<PlayableCard> hand = players.get(username).getHand();
         List<String> res = new ArrayList<>();
@@ -76,8 +74,6 @@ public class Controller implements ControllerInterface {
         }
         clients.get(username).showHand(res);
     }
-
-    @Override
     public void drawGold(String username) throws RemoteException {
         this.players.get(username).drawGold();
 
