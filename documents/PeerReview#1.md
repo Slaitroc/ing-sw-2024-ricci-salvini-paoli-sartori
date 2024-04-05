96e2701276fa76b96e02c6e94164233cdc5c9e0b
Slaitroc <92638923+Slaitroc@users.noreply.github.com>
2024-04-05 12:40:04 +0200

    Refactor for IDEs compatibility, Deleted Controller Interface

--- documents/PeerReview#1.md
+++ documents/PeerReview#1.md
@@ -18,22 +18,22 @@ Valutazione del diagramma UML delle classi del gruppo 21.
 
 - La gestione delle risorse tramite l'oggetto Corner potrebbe essere semplificata usando una lista di Resource e utilizzando l'indice per identificare le posizioni delle risorse nella carta. In questo modo sarebbe riutilizzabile anche per le StarterCard.
 
-- getNeededResources(), presente solamente in GoldCard ne rende articolato l'utilizzo. Ad esempio per effettuare una chiamata da un elemento generico di handsCard:Card[] sembra necessario verificarne il tipo con instanceof.
+- `getNeededResources()`, presente solamente in GoldCard ne rende articolato l'utilizzo. Ad esempio per effettuare una chiamata da un elemento generico di `handsCard`:Card[] sembra necessario verificarne il tipo con `instanceof`.
 
-- L'utilizzo di Resource.CORNER per rappresentare la sfida delle GoldCard e il calcolo dei punti ad essa associati potrebbe essere gestita sempre tramite strategy pattern.
+- L'utilizzo di `Resource.CORNER` per rappresentare la sfida delle GoldCard e il calcolo dei punti ad essa associati potrebbe essere gestita sempre tramite strategy pattern.
 
 #### Strategy Pattern e ObjectiveCard
 
-- calculatePoints() è un metodo di ObjectiveCard e dell'interfaccia GoalStrategy, quindi al chiamante sono disponibili
+- `calculatePoints()` è un metodo di ObjectiveCard e dell'interfaccia GoalStrategy, quindi al chiamante sono disponibili
   `positionCard.calculatePoints()` e `positionCard.goalStrategy.calculatePoints()` creando un'ambiguità non strettamente necessaria.
-  Sebbene questa situazione sia risolvibile impostando l'attributo goalStrategy di PositionCard come privato una soluzione che semplifica ulteriormente il codice potrebbe essere utilizzare un attributo che implementa GoalStrategy direttamente nella classe astratta ObjectiveCard.
-  In questo modo si possono gestire le varie implementazioni di calculatePoints() evitando di creare due diversi livelli di ereditarietà.
+  Sebbene questa situazione sia risolvibile impostando l'attributo `goalStrategy` di PositionCard come privato una soluzione che semplifica ulteriormente il codice potrebbe essere utilizzare un attributo che implementa GoalStrategy direttamente nella classe astratta ObjectiveCard.
+  In questo modo si possono gestire le varie implementazioni di `calculatePoints()` evitando di creare due diversi livelli di ereditarietà.
   WiseManCard e KingdomCard diventerebbero due ulteriori implementazioni di GoalStrategy.
 
 #### Field
 
 - Racchiudere tutti gli attributi `private count*` in una `Map<Resource,Integer>`
-- Un modo più agevole di getPlayedCard() e getPlayedPosition() per gestire le carte presenti nella mappa playArea potrebbe essere restituire direttamente la mappa, o una sua copia, al chiamante che potrebbe a quel punto usare i metodi di `Map`.
+- Un modo più agevole di `getPlayedCard()` e `getPlayedPosition()` per gestire le carte presenti nella mappa playArea potrebbe essere restituire direttamente la mappa, o una sua copia, al chiamante che potrebbe a quel punto usare i metodi di `Map`.
 
 È consigliabile aggiungere i packages per aumentare la leggibilità dell'UML.
 
@@ -45,4 +45,4 @@ Le architetture sono simili in molti punti. Le principali differenze sono:
 - abbiamo creato una classe Deck che crea le carte e le mischia. Inoltre contiene la logica che permette di pescare. Sebbene questa scelta sia più vicina al paradigma OO, la gestione dei deck vista in questo UML potrebbe essere più semplice e immediata.
 - L'architettura dell'oggetto Card è differente in quanto abbiamo scelto di creare classi apposite per i due lati che presentano caratteristiche comuni nelle varie carte.
 - È diversa anche la gestione delle risorse all'interno delle carte, per le quali abbiamo utilizzato una lista dove ad ogni indice corrisponde una posizione sulla carta: da 0 a 3 sono gli angoli, da 4 a 6 le risorse centrali se presenti.
-- Per la costruzione delle carte ci siamo affidati alla libreria Gson,in questo modo possiamo creare gli oggetti delle carte da un file json tramite deserializzazione. Dall'UML analizzato non è chiaro come questo avvenga e potrebbe dunque esserci una differenza.
+- Per la costruzione delle carte ci siamo affidati alla libreria Gson, in questo modo possiamo creare gli oggetti delle carte da un file json tramite deserializzazione. Dall'UML analizzato non è chiaro come questo avvenga e potrebbe dunque esserci una differenza.
--- src/main/java/it/polimi/ingsw/gc31/Server.java
+++ src/main/java/it/polimi/ingsw/gc31/Server.java
@@ -1,13 +1,13 @@
 package it.polimi.ingsw.gc31;
 
-import java.util.ArrayList;
-import java.util.List;
+import it.polimi.ingsw.gc31.rmi.RmiServer;
+import it.polimi.ingsw.gc31.rmi.VirtualServer;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
-import it.polimi.ingsw.gc31.controller.Controller;
-import it.polimi.ingsw.gc31.controller.GameController;
-import it.polimi.ingsw.gc31.model.exceptions.*;
-import it.polimi.ingsw.gc31.model.GameModel;
-import it.polimi.ingsw.gc31.view.GameView;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 
 /*import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -17,98 +17,26 @@ import javafx.stage.Stage;
 import java.io.IOException;*/
 
 public class Server /* extends Application */ {
-    // /*
-    // * @Override
-    // * public void start(Stage stage) throws IOException {
-    // * FXMLLoader fxmlLoader = new
-    // * FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-    // * Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-    // * stage.setTitle("Hello!");
-    // * stage.setScene(scene);
-    // * stage.show();
-    // * }
-    // */
-    //
-    // //private static final Controller controller = new Controller();
-    //
-    //// public static Controller getController() {
-    //// return controller.deepCopy();
-    //// }
-    //
-    // public static void main(String[] args) {
-    // /* launch(); */
-    //
-    // // aggiungo il player1 al Controller
-    // String player1 = "Christian";
-    // addPlayerToController(player1);
-    //
-    // // creo il GameController corrispondente al player1
-    // int numPlayer = 2;
-    // GameController gameController = createGameController(player1, numPlayer);
-    //
-    // // aggiungo il player2 al GameController già creato
-    // String player2 = "Matteo";
-    // addPlayerToGameController(gameController, player2);
-    //
-    // // creo il gameModel
-    // createGameModel(gameController);
-    //
-    // }
-    //
-    // // NOTE for testing
-    // public static void addPlayerToController(String player) {
-    // // aggiungo gli username al controller (che verifica se esistono già
-    // lanciando
-    // // se necessario un'eccezione)
-    // try {
-    // controller.addPlayerUsername(player);
-    // } catch (PlayerNicknameAlreadyExistsException e) {
-    // e.printStackTrace();
-    // }
-    //
-    // }
-    //
-    // public static GameController createGameController(List<String> usernamesList,
-    // int numPlayers) {
-    // if (usernamesList != null && !usernamesList.isEmpty()) {
-    //
-    // int gameControllerID = controller.createGameController(usernamesList.get(0),
-    // numPlayers);
-    // GameController gameController =
-    // controller.getGameController(gameControllerID);
-    // return gameController;
-    // } else
-    // return null;
-    // }
-    //
-    // public static GameController createGameController(String player, int
-    // numPlayers) {
-    // int gameControllerID = controller.createGameController(player, numPlayers);
-    // GameController gameController =
-    // controller.getGameController(gameControllerID);
-    // return gameController;
-    // }
-    //
-    // public static void addPlayerToGameController(GameController gameController,
-    // String player) {
-    // try {
-    // gameController.addPlayer(player);
-    // } catch (MaxPlayerNumberReachedException e) {
-    // e.printStackTrace();
-    // } catch (PlayerNicknameAlreadyExistsException c) {
-    // c.printStackTrace();
-    // }
-    // }
-    //
-    // public static void createGameModel(GameController gameController) {
-    // try {
-    // gameController.createGameModel();
-    // } catch (PlayerNumberNotReachedException e) {
-    // System.out.println("Player Number Not Reached!");
-    // e.printStackTrace();
-    // } catch (GameModelAlreadyCreatedException e) {
-    // System.out.println("Game Model Alredy Created!");
-    // e.printStackTrace();
-    // }
-    // }
+    /*
+     * @Override
+     * public void start(Stage stage) throws IOException {
+     * FXMLLoader fxmlLoader = new
+     * FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+     * Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+     * stage.setTitle("Hello!");
+     * stage.setScene(scene);
+     * stage.show();
+     * }
+     */
+
+    public static void main(String[] args) throws RemoteException {
+        String name = "VirtualServer";
+        VirtualServer engine = new RmiServer();
+        VirtualServer stub = (VirtualServer) UnicastRemoteObject.exportObject(engine, 0);
+        Registry registry = LocateRegistry.createRegistry(1234);
+        registry.rebind(name, stub);
+
+        System.out.println("[SERVER] Adder bound");
+
+    }
 }
