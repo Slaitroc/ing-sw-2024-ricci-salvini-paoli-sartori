48b5b1f8c4a99cc35e02fdf1ad95c5dbdb95913c
salvo <salvoc878@gmail.com>
2024-04-19 15:04:36 +0100

    Skeleton of GameModel States

---
+++ src/main/java/it/polimi/ingsw/gc31/model/GameSetUp.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.gc31.model;
+
+public class GameSetUp implements GameState{
+    @Override
+    public void startGame() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void endGame() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void runGame() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
