0e0d0c105e8d0a38a66046d8ed63010cd8ead07f
Slaitroc <92638923+Slaitroc@users.noreply.github.com>
2024-05-04 13:34:42 +0200

    Add UML Peer Review 2

--- src/main/java/it/polimi/ingsw/gc31/utility/FileUtility.java
+++ src/main/java/it/polimi/ingsw/gc31/utility/FileUtility.java
@@ -1,4 +1,4 @@
-package it.polimi.ingsw.gc31.utility.file;
+package it.polimi.ingsw.gc31.utility;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -6,12 +6,11 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class FileUtility {
-    
 
-    public static String fileToString(String path){
+    public static String fileToString(String path) {
         String userDir = System.getProperty("user.dir");
         String relativePath = path;
-        
+
         StringBuilder stringBuilder = new StringBuilder();
         try (BufferedReader br = new BufferedReader(new FileReader(userDir + File.separator + relativePath))) {
             String line;
