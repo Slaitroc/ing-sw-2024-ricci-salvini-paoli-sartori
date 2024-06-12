03138ce1a027bc4724b51a61231bc556f100e78d
Krotox <matteog.paoli@gmail.com>
2024-06-12 23:37:53 +0200

    General fixes

--- src/main/java/it/polimi/ingsw/gc31/model/gameModel/SetupGameModelState.java
+++ src/main/java/it/polimi/ingsw/gc31/model/gameModel/SetupGameModelState.java
@@ -3,16 +3,18 @@ package it.polimi.ingsw.gc31.model.gameModel;
 import it.polimi.ingsw.gc31.client_server.interfaces.VirtualClient;
 import it.polimi.ingsw.gc31.exceptions.IllegalStateOperationException;
 import it.polimi.ingsw.gc31.exceptions.ObjectiveCardNotChosenException;
+import it.polimi.ingsw.gc31.exceptions.WrongIndexSelectedCard;
 import it.polimi.ingsw.gc31.model.player.Player;
 
 import java.awt.*;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class SetupGameModelState implements GameModelState{
+public class SetupGameModelState implements GameModelState {
     public SetupGameModelState() {
         System.out.println("Game changed to SETUP");
     }
+
     @Override
     public Map<String, Player> initGame(GameModel model, LinkedHashMap<String, VirtualClient> clients) throws IllegalStateOperationException {
         throw new IllegalStateOperationException();
@@ -28,7 +30,7 @@ public class SetupGameModelState implements GameModelState{
         model.getPlayers().get(username).playStarter();
 
         boolean allPlayersPlayedStarter = true;
-        for (Player player: model.getPlayers().values()) {
+        for (Player player : model.getPlayers().values()) {
             if (player.infoState().equals("start")) {
                 allPlayersPlayedStarter = false;
             }
@@ -56,13 +58,13 @@ public class SetupGameModelState implements GameModelState{
     }
 
     @Override
-    public void setSelectCard(GameModel model, String username, int index) throws IllegalStateOperationException {
-        throw new IllegalStateOperationException();
+    public void setSelectCard(GameModel model, String username, int index) throws IllegalStateOperationException, WrongIndexSelectedCard {
+        model.getPlayers().get(username).setSelectedCard(index);
     }
 
     @Override
     public void changeSide(GameModel model, String username) throws IllegalStateOperationException {
-        throw new IllegalStateOperationException();
+        model.getPlayers().get(username).changeSide();
     }
 
     @Override
