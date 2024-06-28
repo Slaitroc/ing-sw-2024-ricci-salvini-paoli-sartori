e742e2552b3fb7b24778c0df783c7ed2cc19cd24
Slaitroc <92638923+Slaitroc@users.noreply.github.com>
2024-06-28 12:27:12 +0200

    File Utility JavaDoc

--- src/main/java/it/polimi/ingsw/gc31/utility/FileUtility.java
+++ src/main/java/it/polimi/ingsw/gc31/utility/FileUtility.java
@@ -13,9 +13,18 @@ import it.polimi.ingsw.gc31.exceptions.NoFileCreatedException;
  */
 public class FileUtility {
 
+    /**
+     * Name of the folder that will be created on the desktop
+     */
     public static final String folderName = "CodexNaturalis";
+    /**
+     * Name of the file that will be created in the {@link #folderName} folder
+     */
     public static final String fileName = "Token.txt";
 
+    /**
+     * @return the path of the desktop
+     */
     public static String getDesktopPath() {
         String userHome = System.getProperty("user.home");
         String osName = System.getProperty("os.name").toLowerCase();
@@ -32,21 +41,39 @@ public class FileUtility {
         return desktopPath;
     }
 
+    /**
+     * @return the path of the folder {@link #folderName} on the desktop
+     */
     public static Path getCodexNaturalisPath() {
         // Crea il percorso completo della cartella e del file
         return Paths.get(getDesktopPath(), folderName);
     }
 
+    /**
+     * @return the path of the file {@link #fileName} in the folder
+     *         {@link #folderName}
+     */
     public static Path getCodexTokenFilePath() {
         // Crea il percorso completo della cartella e del file
         return Paths.get(getDesktopPath(), folderName, fileName);
     }
 
+    /**
+     * @param path the path to check
+     * @return true if the path exists, false otherwise
+     */
     public static boolean exists(Path path) {
         boolean exists = Files.exists(path);
         return exists;
     }
 
+    /**
+     * 
+     * @param path        the path to create
+     * @param folderName  the name of the folder to create
+     * @param desktopPath if true the folder will be created on the desktop
+     * @throws NoFileCreatedException
+     */
     public static void createDirectory(Path path, String folderName, boolean desktopPath)
             throws NoFileCreatedException {
         try {
